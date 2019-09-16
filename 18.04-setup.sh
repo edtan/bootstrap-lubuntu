48f5ee13498dbfa559fc2f222217a7fb4d74e4af
Ed Tan <edtan@users.noreply.github.com>
2019-09-16 12:29:32 -0400

    Install i3 window manager

--- 18.04-setup.sh
+++ 18.04-setup.sh
@@ -75,6 +75,14 @@ git config --global user.email edtan@users.noreply.github.com
 mkdir -p ~/.config/autostart
 cp synapse.desktop ~/.config/autostart/synapse.desktop
 
+# i3 window manager (https://i3wm.org/docs/repositories.html)
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+sudo dpkg -i ./keyring.deb
+rm keyring.deb
+echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee --append /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install i3
+
 # get keyboard shortcut mappings
 # https://askubuntu.com/a/306172 -> window tiling
 # TODO: merge keybindings.xml into lubuntu-rc.xml
